c688e6dbfdd556f8cfe20027192eb94489de8603
ShahriarSifat <shahriarsifat25@gmail.com>
2022-06-21 13:30:59 +0600

    .

---
+++ Labexm/Registration.php
@@ -0,0 +1,18 @@
+
+
+<html>
+<head>
+	<title>REGISTRATION</title>
+</head>
+<body>
+	<form action="regcheck.php" method="post" enctype="">
+		<fieldset>
+			<legend>Signup</legend>
+				Username: <input type="text" name="username" value=""/> <br>
+				Password: <input type="password" name="password" value=""/> <br>
+				Email: <input type="email" name="email" value=""/> <br>
+				<input type="submit" name="submit" value="Submit"/>
+		</fieldset>
+	</form>
+</body>
+</html>
